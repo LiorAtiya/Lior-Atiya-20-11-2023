92c2333a1d1c4a43ed549267878246de98fb439e
Lior <lior4007@gmail.com>
2023-11-24 02:26:02 +0200

    change minor styles

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -3,10 +3,10 @@ import React from "react";
 function SearchResults({ citiesList, chooseCity }) {
   return (
     <div className="text-center">
-      <h1 className="mt-3 text-lg font-bold">Search Results:</h1>
+      <h1 className="mt-3 text-lg font-bold dark:text-white">Search Results:</h1>
       {citiesList?.map((item, index) => {
         return (
-          <button onClick={() => chooseCity(item.Key, item.LocalizedName)} className="w-1/4 p-4 m-5 text-center bg-blue-100 rounded-md">
+          <button onClick={() => chooseCity(item.Key, item.LocalizedName)} className="w-1/4 p-4 m-5 text-center bg-teal-100 rounded-md">
             {item.LocalizedName}
           </button>
         );
